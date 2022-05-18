611f95bbc4d7877700d022bbbdaf3edcefa64fa9
McCall Woodward <woodwardem@g.crookcountyschools.org>
2022-05-18 08:49:02 -0700

    added an about page and added the nav bar

---
+++ src/About.js
@@ -0,0 +1,21 @@
+import { AccordionButton } from "react-bootstrap";
+import React,  { Component }  from "react";
+
+
+class About extends Component {
+    render() {
+        return (
+            <>
+        <p>This application gives the user the ability to search for and make a 
+            collection of video games they enjoy or like to enjoy.
+        </p>
+        <h2>Our Team: 
+            McCall: frontend
+            Layton: frontend
+            Marissa: frontend
+        </h2>
+        </>
+         ) }
+};
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+
 import Header from './Header';
+
 import React from 'react';
 import Footer from './Footer';
 import Main from './Main';
+import About from './About';
+
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <Router>
         <Header />
-        <Main />
+        <Routes>
+          <Route exact path='/'
+            element={<Main />} ></Route>
+          <Route path='/About'
+            element={<About />} ></Route>
+
+        </Routes>
         <Footer />
-      </>
+      </Router>
     );
   }
 }
