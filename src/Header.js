6fb014d0eef3578c26875491b32dcbc7531672a4
McCall Woodward <woodwardem@g.crookcountyschools.org>
2022-05-20 08:49:17 -0700

    added a carousel and started to connect to the database

--- src/Header.js
+++ src/Header.js
@@ -9,6 +9,7 @@ class Header extends React.Component {
             <Navbar.Brand>Videogame Finder</Navbar.Brand>
             <NavItem><Link to='/' className="nav-link">Home</Link></NavItem>
             <NavItem><Link to='/About'className="nav-link">About</Link></NavItem>
+            <NavItem><Link to='/FavoriteGames'className="nav-link">WishList</Link></NavItem>
             {/* PLACEHOLDER: render a navigation link to the about page */}
           </Navbar>
         ) 
--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,44 @@
+import axios from "axios";
 import React from "react";
 import FavoriteGames from "./FavoriteGames";
 
 class Main extends React.Component{ 
+ 
+  constructor(props) {
+    super(props);
+    this.state= {
+game: ' '
+    }
+  }
+
+
+
+
+onChange = (input) => {
+let getGames = input.target.value;
+this.setState({game: getGames});
+console.log(getGames);
+};
+
+onSubmit = async (event) => {
+  event.preventDefault();
+  console.log(' made it inside onSubmit ');
+const url = `${process.env.REACT_APP_SERVER}/games?gameName=${this.state.game}`;
+console.log(url);
+//const response = await axios.get(url);
+//
+//console.log(response.data);
+
+}
+
     render() {
          return(
            <>
            
-            <form className='form' onSubmit={this.getLocation}>
+            <form className='form' onSubmit={(event ) => this.onSubmit(event)}>
 
             Your Search: {" "}
-            <input type="text" name="yourgame" onChange={this.handleChange} />
+            <input type="text" name="yourgame" onChange={this.onChange} />
             <button type='submit'>Search</button>
           </form>
 
@@ -22,6 +51,28 @@ class Main extends React.Component{
         
          
         }
+        
+        <Container >
+            <Carousel>
+              {this.state.game.map(game=> (
+                <Carousel.Item key={book._id}>
+                  <img
+                   // id='carousel-image'
+                   // className="d-block w-100"
+                  //  src={gameImg}
+                    alt={game.title}
+                  />
+
+                  <Carousel.Caption id='carousel-text-box'>
+                    <h3>{game.title}</h3>
+                    <p>{game.description}</p>
+                    <p>Release Date: {game.releaseDate}</p>
+                  <Button onClick= {() => this.deleteGame(game)}>Delete</Button>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </Container>
     
     }
 
